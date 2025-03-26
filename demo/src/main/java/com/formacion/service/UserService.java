3b317b968c3db2cb465a01173f37baf4dedd2664
Loren-SMServices <laurentzi.sm@gmail.com>
2025-03-26 16:20:53 +0100

    Añadida codificacion y verificacion de pass cifrada

--- demo/src/main/java/com/formacion/service/UserService.java
+++ demo/src/main/java/com/formacion/service/UserService.java
@@ -2,6 +2,7 @@ package com.formacion.service;
 
 import java.util.List;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.formacion.entities.User;
@@ -10,10 +11,12 @@ import com.formacion.repository.UserRepository;
 @Service
 public class UserService {
 
-    private final UserRepository userRepository;
+	private final UserRepository userRepository;
+	private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public User findUserByUsername(String username) {
@@ -21,10 +24,32 @@ public class UserService {
     }
 
     public User saveUser(User user) {
+    	// Codificar la contraseña antes de guardar
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
     
     public List<User> findAllUsers() {
         return userRepository.findAll();
     }
+    
+    public Long countUsers() {
+        return userRepository.count();
+    }
+    
+ // Nuevo método para verificar credenciales
+    public boolean verifyUserCredentials(String username, String rawPassword) {
+        User user = userRepository.findByUsername(username);
+        if (user != null) {
+            // Compara la contraseña ingresada con la encriptada almacenada
+            return passwordEncoder.matches(rawPassword, user.getPassword());
+        }
+        return false;
+    }
+
+    public boolean verifyPassword(String rawPassword, String encodedPassword) {
+        return passwordEncoder.matches(rawPassword, encodedPassword);
+    }
+
+    
 }
