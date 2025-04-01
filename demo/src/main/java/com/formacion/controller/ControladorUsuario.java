51d9acf07df8a6cdf2c09393d3f45e0cc723495d
Loren-SMServices <laurentzi.sm@gmail.com>
2025-04-01 16:47:21 +0200

    Añadido metodo para recoger los datos recibidos de principal.jsp

--- demo/src/main/java/com/formacion/controller/ControladorUsuario.java
+++ demo/src/main/java/com/formacion/controller/ControladorUsuario.java
@@ -1,12 +1,31 @@
 package com.formacion.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import lombok.extern.java.Log;
 
 @Controller
 @RequestMapping("/usuario")
+@Log
 public class ControladorUsuario {
 	
+	
+	@PostMapping("/updateUsuario")
+	public ResponseEntity<String> updateUsuario(@RequestParam int id,
+            @RequestParam String firstName,
+            @RequestParam String lastName,
+            @RequestParam String image) {
+
+
+		log.info("Llegan " + id + " : " + firstName);
+		// Devuelve una respuesta de éxito
+        return ResponseEntity.ok("Actualizado correctamente.");
+
+	}
 	
 
 }
