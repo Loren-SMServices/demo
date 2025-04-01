1b0e4a7a8c8e7261ff38072e34e6d50e45a77e21
Loren-SMServices <laurentzi.sm@gmail.com>
2025-04-01 11:45:33 +0200

    Nuevo Controlador para usuarios "Normales"

---
+++ demo/src/main/java/com/formacion/controller/ControladorUsuario.java
@@ -0,0 +1,12 @@
+package com.formacion.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/usuario")
+public class ControladorUsuario {
+	
+	
+
+}
