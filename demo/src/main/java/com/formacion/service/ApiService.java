30d110b6340efe768f083e5c97c2f52c937432df
Loren-SMServices <laurentzi.sm@gmail.com>
2025-04-01 13:57:49 +0200

    conversion de datos a objeto

--- demo/src/main/java/com/formacion/service/ApiService.java
+++ demo/src/main/java/com/formacion/service/ApiService.java
@@ -1,25 +1,44 @@
 package com.formacion.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.formacion.entities.UserApiResponse;
+import com.formacion.entities.UserApiResponse.User;
+
+import lombok.extern.java.Log;
+
 @Service
+@Log
 public class ApiService {
 
-	
-	private final WebClient webClient;
+    private final WebClient webClient;
 
     public ApiService(WebClient webClient) {
         this.webClient = webClient;
     }
 
-    public String getApiData() {
-        String url = "https://dummyjson.com/users/1"; // URL de la API
-        return webClient.get()
+    public List<User> getApiDataList() {
+        String url = "https://dummyjson.com/users"; // URL de la API
+        String response = webClient.get()
                 .uri(url)
                 .retrieve()
-                .bodyToMono(String.class)
-                .block(); // Bloquea para obtener el resultado (no reactivo)
+                .bodyToMono(String.class) // Recibe como texto
+                .block();
+        log.info(response);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            // Convierte el JSON al objeto `UsersResponse`
+            UserApiResponse usersResponse = objectMapper.readValue(response, UserApiResponse.class);
+            
+            return usersResponse.getUsers(); // Devuelve la lista de usuarios
+        } catch (Exception e) {
+            throw new RuntimeException("Error al mapear el JSON a UsersResponse.", e);
+        }
     }
-	
 }
+
