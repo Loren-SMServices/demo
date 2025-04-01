b7a79e1fd5cf7ba681ed0a34aaac0e876a3861b8
Loren-SMServices <laurentzi.sm@gmail.com>
2025-04-01 11:40:57 +0200

    Implementamos funcionalidad para que Spring Security reconozca el usuario y lo almacene en memoria

--- demo/src/main/java/com/formacion/entities/User.java
+++ demo/src/main/java/com/formacion/entities/User.java
@@ -1,5 +1,12 @@
 package com.formacion.entities;
 
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -10,14 +17,20 @@ import lombok.Data;
 @Entity
 @Data
 @Table(name = "usuarios")
-public class User {
-	
+public class User implements UserDetails{
+
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
     private String username; // Email o nombre de usuario
     private String password; // Contraseña
     private String role;     // Rol del usuario
-
+	
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority("ROLE_" + role));
+    }
 
 }
