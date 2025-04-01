81b99862e954112e3e2cd717f4d1b1e55e356c57
Loren-SMServices <laurentzi.sm@gmail.com>
2025-04-01 11:50:00 +0200

    Añadida ruta adicional /api para el controlador

--- demo/src/main/java/com/formacion/controller/rest/ApiController.java
+++ demo/src/main/java/com/formacion/controller/rest/ApiController.java
@@ -4,16 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.formacion.service.ApiService;
 
 @Controller
+@RequestMapping("/api")
 public class ApiController {
 	
 	@Autowired
     private ApiService apiService;
 
-    @GetMapping("/api-data")
+    @GetMapping("/data")
     public String fetchApiData(Model model) {
     	String apiData = apiService.getApiData();
     	System.out.println(apiData);
