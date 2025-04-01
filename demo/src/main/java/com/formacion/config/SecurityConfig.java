471257bead151a22c5b5144f77b5c169dff4b8de
Loren-SMServices <laurentzi.sm@gmail.com>
2025-04-01 11:43:30 +0200

    configuracion del metodo securityFilterChain que utilizara Spring Security para simplificar el acceso a las vistas y gestionara los usuarios y sus permisos

--- demo/src/main/java/com/formacion/config/SecurityConfig.java
+++ demo/src/main/java/com/formacion/config/SecurityConfig.java
@@ -7,24 +7,45 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-public class SecurityConfig {
-	
-	@Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable()) // Deshabilitar CSRF si no lo necesitas
-                .authorizeHttpRequests(authorize -> authorize
-                                .anyRequest().permitAll() // Permitir todas las solicitudes sin autenticación
-                );
+import lombok.extern.java.Log;
 
-        return http.build();
-    }
-	
-	@Bean
+@Configuration
+@Log
+public class SecurityConfig {
+
+    @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    	return http
+                .authorizeHttpRequests(auth -> auth
+                		.requestMatchers("/anonimo/**").permitAll() //Permite paso a cualquier clase del controlador principal
+//                		.requestMatchers("/admin/**").hasRole("ADMIN") // Sólo usuarios con rol ADMIN
+                        .requestMatchers("/api/**", "/usuario/**").authenticated() // Paso a usuarios autenticados
+//                        .requestMatchers("/usuario/**").hasAnyRole("USER", "ADMIN") // Paso a controlador para usuarios con roles USER o ADMIN 
+                		.anyRequest().permitAll()
+                )
+                .formLogin(form -> form
+                	    .loginPage("/anonimo/login") // Página de login
+                	    .loginProcessingUrl("/anonimo/formularioLogin") // Endpoint para procesar el login
+                	    .failureUrl("/anonimo/login?error=true") // En caso de fallo
+                	    .defaultSuccessUrl("/api/data", true) // En caso de éxito
+                	    .permitAll()
+                	)
+                .logout(logout -> logout
+                	    .logoutUrl("/logout")
+                	    .logoutSuccessHandler((request, response, authentication) -> {
+                            // Lógica personalizada después del logout
+                            log.info("Usuario cerró sesión: " + (authentication != null ? authentication.getName() : "Anonimo"));
+                            response.sendRedirect("/anonimo/"); // Redirección a la raiz
+                        })
+
+                )
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/**"))
+                .build();
+    }
 
 }
