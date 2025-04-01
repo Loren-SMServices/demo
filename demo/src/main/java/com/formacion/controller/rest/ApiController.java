7452bf8bbccd53c22352b5055c21bdee5ed4b4c8
Loren-SMServices <laurentzi.sm@gmail.com>
2025-04-01 13:58:06 +0200

    Paso de objeto a la vista

--- demo/src/main/java/com/formacion/controller/rest/ApiController.java
+++ demo/src/main/java/com/formacion/controller/rest/ApiController.java
@@ -1,27 +1,31 @@
 package com.formacion.controller.rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.formacion.entities.UserApiResponse.User;
 import com.formacion.service.ApiService;
 
+import lombok.extern.java.Log;
+
 @Controller
+@Log
 @RequestMapping("/api")
 public class ApiController {
 	
 	@Autowired
     private ApiService apiService;
 
-    @GetMapping("/data")
-    public String fetchApiData(Model model) {
-    	String apiData = apiService.getApiData();
-    	System.out.println(apiData);
-        model.addAttribute("apiData", apiData); // Añade los datos al modelo
-        return "principal"; // Nombre del archivo JSP (sin extensión)
-
-    }
+	@GetMapping("/data")
+	public String fetchApiData(Model model) {
+	    List<User> lista = apiService.getApiDataList();
+	    model.addAttribute("lista", lista); // Envía la lista a la vista
+	    return "principal";
+	}
 
 }
